a0eeea5156f0db68a9e439ba226d9d2c6a479e80
agisko <agisko@sigma.ug.pl>
2015-04-12 01:10:38 +0200

    update ak

--- petla_ak.c
+++ petla_ak.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+   // deklaracja zmiennych
+   int i;
+   printf("wyswietlanie znakow a-k w kolejnosci rosnacej\n");
+   // iterowanie w kolejnosci rosnacej
+   for(i = (int)'a'; i <= (int)'k'; i++)
+   {
+      printf("%c -> kod: %d(dec), 0x%x\n", (char)i, i, i);
+   }
+
+   printf("wyswietlanie znakow a-k w kolejnosci malejacej\n");
+   // iterowanie w kolejnosci malejacej
+   for(i = (int)'k'; i >= (int)'a'; i--)
+   {
+      printf("%c -> kod: %d(dec), 0x%x\n", (char)i, i, i);
+   }
+     
+   system("PAUSE");	
+   return 0;
+}
